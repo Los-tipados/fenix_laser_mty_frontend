a779164c0ce258c09026f99fcf4d1e724eac3dbc
z-erik <erikp2913@gmail.com>
2026-01-29 20:56:40 -0600

    feat: se crea el script config.js que se encarga de gestionar las fuentes, productos y figuras

---
+++ activos/js/logica-editor/config.js
@@ -0,0 +1,55 @@
+export const CONFIG = {
+    menu: [
+        { id: 'texto', icon: 'A', label: 'Texto' },
+        { id: 'productos', icon: '📦', label: 'Productos' },
+        { id: 'figuras', icon: '⬔', label: 'Figuras' }
+    ],
+    // Array de fuentes actualizado con las de Google Fonts + Estándar
+    fuentes: [
+        /* --- ESTILO URBANO Y RARO --- */
+        'Bungee',           // Bloques pesados, muy urbana
+        'Permanent Marker',
+        
+        /* --- ESTILO MODERNO Y TECH --- */
+        'Righteous',        // Art-deco moderna y muy legible
+        'Orbitron',         // Estilo ciencia ficción / gaming
+        'Comfortaa',        // Redonda, amable y muy minimalista
+        
+        /* --- ESTILO ELEGANTE Y CLÁSICO --- */
+        'Abril Fatface',    // Serif de alto contraste (estilo revista de moda)
+        'Playfair Display', // Elegancia pura
+        'Fascinate Inline', // Estilo Broadway/Cine antiguo
+        
+        /* --- ESTILO DIVERTIDO Y RETRO --- */
+        'Pacifico',         // Script/Cursiva de los años 50 suave
+        'Press Start 2P',   // Pixel art de consola de 8 bits
+        'Montserrat',       // La reina de la versatilidad moderna
+        
+        /* --- BÁSICAS --- */
+        'Arial', 
+        'Impact', 
+        'Georgia'
+    ],
+    productos: [
+        { id: 'p1', nombre: 'Caja', url: 'https://i.ibb.co/JwT7G5nb/box-real.webp'},
+        { id: 'p2', nombre: 'Cuchillo', url: 'https://i.ibb.co/WvXx1qW6/box.webp' },
+        { id: 'p3', nombre: 'Gorra Eco', url: 'https://via.placeholder.com/150/e67e22/FFFFFF?text=Gorra' }
+    ],
+    figuras: [
+        { nombre: 'Loro', url: 'https://i.ibb.co/QjjGXwpV/animal-1295493-1280.png' },
+        { nombre: 'Panda', url: 'https://i.ibb.co/BHk0rYYd/panda.png' },
+        { nombre: 'Corazon', url: 'https://i.ibb.co/PGRR9QZ3/Corazon.png' },
+        { nombre: 'Caballo', url: 'https://i.ibb.co/MkYrL1jv/horse-3254102-1280.png' },
+
+        { nombre: 'Auto', url: 'https://i.ibb.co/M5DR24PT/carro-oldie.png'},
+        
+        { nombre: 'Marco', url: 'https://i.ibb.co/DZH52CB/marco1.png'},
+        
+        { nombre: 'Corona', url: 'https://i.ibb.co/4R762cYd/corona.png'},
+        { nombre: 'Corazoncitos', url: 'https://i.ibb.co/FL1gyMNj/corazoncitos.png'},
+        { nombre: 'Rosa', url: 'https://i.ibb.co/bgNPX1wM/ai-generated-9166476-1280.png'},
+        { nombre: 'Divisor', url: 'https://i.ibb.co/Kcv62TFt/divider-9999292-640.png'},
+        
+        { nombre: 'Espada', url: 'https://i.ibb.co/4nDSX19S/espada.png'}
+    ]
+};
