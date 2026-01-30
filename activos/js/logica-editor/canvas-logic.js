a4e0c04a4de30d1e3fe67dcd751d9e3be0f725ff
z-erik <erikp2913@gmail.com>
2026-01-29 20:39:51 -0600

    feat: se crea el modulo CanvasLogic y creamos una instancia global fabric.Canvas

---
+++ activos/js/logica-editor/canvas-logic.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * @module CanvasLogic
+ * @description Operaciones de bajo nivel para la manipulación del lienzo de Fabric.js.
+ * Este módulo actúa como la capa de servicio entre la librería y la interfaz de usuario.
+ */
+
+/** @type {fabric.Canvas} Instancia global del lienzo */
+export let canvas;
+
